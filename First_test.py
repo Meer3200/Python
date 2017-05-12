dd29060b8368a3df0d834e800afe7686ef982097
Meer3200 <meteorgrey@e-mail.ua>
2017-05-12 14:45:37 +0300

    Removed superfluous

--- First_test.py
+++ First_test.py
@@ -1,31 +1,5 @@
-'''def func(a, b):
-    i = a
-    while i < b:
-        if i%2 == 0:
-            print(i)
-        i+=1
 
-x = func
-x(34, 37)'''
-
-'''def ariphmetic(*args):
-    res = 0
-    a = float(input("Введите a"))
-    b = float(input("Введите b"))
-    c = input("Введите действие")
-    if c == "+":
-        res = a + b
-    elif c == "-":
-        res = a - b
-    elif c == "*":
-        res = a * b
-    elif c == "/":
-        res = a / b
-    else:
-        print("Неизвестная операция")
-    print("Результат:", res)
-ariphmetic()'''
-
+# ..........................LEAP YEAR (version 1).......................
 '''def is_year_leap(*args):
     a = int(input("Введите год"))
     def recurs(a):
@@ -54,7 +28,9 @@ ariphmetic()'''
     return recurs(a)
 
 is_year_leap()'''
+# ---------------------------------------------------------------------
 
+# ........................LEAP YEAR (version 2).........................
 '''def is_year_leap(*args):
     a = int(input("Введите год"))
     b = a
@@ -73,7 +49,9 @@ is_year_leap()'''
         else:
             print("Год " + str(b) + " - обычный")
 is_year_leap()'''
+# ----------------------------------------------------------------------
 
+# .......................LEAP YEAR (elegantly).......................
 '''a = int(input("Введите год"))
 def is_year_leap(a):
     if a > 0:
@@ -86,17 +64,9 @@ def is_year_leap(a):
             print("Обычный")
 
 is_year_leap(a)'''
+# ----------------------------------------------------------------------
 
-'''import math
-def square(*args):
-    a = int(input("Please, enter a square side: "))
-    p = 4*a
-    s = a**2
-    d = round(math.sqrt(a**2 + a**2), 2)
-    res = ("Perimeter = {}".format(p), "Square = {}".format(s), "Diagonal = {}".format(d))
-    print(res)
-square()'''
-
+# ................Encrypting a string (aaabb = 3a2b).................
 '''def encrypt (*args):
     s = input("Введите строку: ")
     num = 0
@@ -117,7 +87,9 @@ square()'''
     return res
 output = encrypt()
 print(output)'''
+# ---------------------------------------------------------------------
 
+# ..................Decrypting a string (3a2b = aaabb)...............
 '''def decrypt (*args):
     s = input("Введите зашифрованую строку: ")
     res = ''
@@ -129,14 +101,19 @@ print(output)'''
         i += 1
     return res
 print(decrypt())'''
+# ----------------------------------------------------------------------
 
-'''def field(*args):
-    x = int(input("Введите ширину: "))
-    y = int(input("Введите высоту: "))
+# ................................SAPPER..............................
+'''
+# Game field creation
+def field(*args):
+    x = int(input("Введите ширину поля игры: "))
+    y = int(input("Введите высоту поля игры: "))
     s = [str(i+1) + "x" + str(j+1) for i in range(x) for j in range(y)]
     return(s)
 s = field()
 
+# Location of mines
 z = int(input("Введите количество мин: "))
 def bom_loc(s):
     i = 1
@@ -153,6 +130,7 @@ def bom_loc(s):
 s_bomb = bom_loc(s)
 print(s_bomb)
 
+# Game process
 def shoot(s):
     i = 0
     while i < z:
@@ -174,45 +152,7 @@ def shoot(s):
             print("Такого значения не существует. Попробуйте значение с этого ряда: " + str(s))
     print("Вы нашли все бомбы!")
 shoot(s)'''
-
-'''def num_row(*args):
-    n = int(input("Введите число: "))
-    s = 0
-    i = 1
-    res = []
-    mid = ''
-    while s <= n:
-        res = res + [i] * i
-        i += 1
-        s = len(res)
-    if s != n:
-        while s > n:
-            res.pop()
-            s -= 1
-    for j in res:
-        mid = mid + str(j)
-    fin = ' '.join(mid)
-    print(fin)
-num_row()'''
+# --------------------------------------------------------------------
 
 
-
-'''def ent_num(*args):
-    with open("../../Python/Test/task.txt") as lst:
-        l = [line.strip() for line in lst]
-        n = l[0].count(l[1])
-        j = 0
-        for i in l[0]:
-            if i == l[1]:
-                print(j, end = ' ')
-            j += 1
-        if n == 0:
-            print("Отсутствует.")
-    'with open("../../Python/Test/task.txt", 'wt') as lst_w:
-        line_wr = input("Новое число: ")
-        lst_w.write[1](line_wr)'
-ent_num()'''
-
-'''fib = lambda x: 1 if x <= 2 else fib(x - 1) + fib(x - 2)
-fib(31)'''
 
